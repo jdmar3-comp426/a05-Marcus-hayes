9b536d6a4a4d4c4aedc58f313a776cf4ee872c51
Marcus Hayes <marcus@antidote.gg>
2021-11-01 19:28:27 -0400

    Initial commit; only user/:id & users endpoints function, according to localhost instance

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 // Define app using express
 var express = require("express")
 var app = express()
+
 // Require database SCRIPT file
+var db = require("./database.js")
 
 // Require md5 MODULE
+var md5 = require("md5")
 
 // Make Express use its own built-in body parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set server port
+const HTTP_PORT = 5000;
 
 // Start server
 app.listen(HTTP_PORT, () => {
@@ -17,12 +21,19 @@ app.listen(HTTP_PORT, () => {
 });
 // READ (HTTP method GET) at root endpoint /app/
 app.get("/app/", (req, res, next) => {
-    res.json({"message":"Your API works! (200)"});
+    res.json({"message":"Your API is working!"});
 	res.status(200);
 });
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
+
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
+app.post("/app/new", (req, res) => {
+	var encrypted_pass = md5(req.body.pass);
+	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?,?)");
+	const info = stmt.run(req.body.user, encrypted_pass);
+	res.status(200).json(info.lastInsertRowid);
+});
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
@@ -31,10 +42,26 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
+app.get("/app/user/:id", (req, res) => {	
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").get(req.params.id);
+	res.status(200).json(stmt);
+});
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
+app.patch("/app/update/user/:id", (req, res) => {
+	var encrypted_pass = md5(req.body.pass);	
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
+	const info = stmt.run(req.body.user, encrypted_pass, req.params.id);
+	res.status(200).json(info.changes);
+});
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
+app.delete("/app/delete/user/:id", (req, res) => {
+	var encrypted_pass = md5(req.body.pass);	
+	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
+	const info = stmt.run(req.body.user, encrypted_pass, req.params.id);
+	res.status(200).json(info.changes);
+});
 
 // Default response for any other request
 app.use(function(req, res){
