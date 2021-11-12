fe750229e07f4e9bb482907bcf1bd0684610ab86
Marcus Hayes <marcus@antidote.gg>
2021-11-11 20:12:33 -0500

    Plez work

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const HTTP_PORT = 5000;
 
 // Start server
 app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+    // console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
 // READ (HTTP method GET) at root endpoint /app/
 app.get("/app/", (req, res) => {
